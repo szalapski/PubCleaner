6b8ad6621e18aa550d5fa652b43fa032ef850460
PSzalapski <szalap@hotmail.com>
2013-12-05 23:22:59 -0600

    built out stubs and unit tests

--- PubCleaner.Lib/CleanResults.cs
+++ PubCleaner.Lib/CleanResults.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace Szalapski.PubCleaner.Lib {
+    
+    /// <summary>
+    /// Represents the results of a clean operation.
+    /// </summary>
     public class CleanResults {
         public CleanResults(bool success) {
             Success = success;
+            _filesCleaned = new List<FileInfo>();
         }
 
         public bool Success { get; set; }
+
+        public List<FileInfo> FilesCleaned {
+            get { return _filesCleaned; }
+        }
+        private List<FileInfo> _filesCleaned;
     }
 }
--- PubCleaner.Lib/Cleaner.cs
+++ PubCleaner.Lib/Cleaner.cs
@@ -1,11 +1,36 @@
-﻿namespace Szalapski.PubCleaner.Lib {
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Szalapski.PubCleaner.Lib {
     public class Cleaner {
+        private IFileStore kindle;
         
+        public Cleaner(IFileStore kindle) {
+            this.kindle = kindle;
+        }
+
         /// <summary>
         /// Cleans all old periodcials/publications from the attached Kindle, leaving only the most recent one
         /// </summary>
         public CleanResults Clean() {
-            return new CleanResults(true);
+            List<FileInfo> periodicals = kindle.GetPeriodicals();
+            List<FileInfo> oldOnes = FilterToOldPeriodicals(periodicals);
+            kindle.Delete(oldOnes);
+            var result = new CleanResults(true);
+            result.FilesCleaned.AddRange(oldOnes);
+            return result;
+        }
+
+        private List<FileInfo> FilterToOldPeriodicals(IEnumerable<FileInfo> periodicals) {
+            if (periodicals == null) throw new ArgumentNullException("periodicals");
+            List<FileInfo> results = new List<FileInfo>();
+            foreach (var file in periodicals.OrderBy( f => f.CreationTime)) {
+                string frontOfFilename = file.Name.Substring(0, file.Name.LastIndexOf('_')+1);
+                if (!results.Any(f => f.Name.StartsWith(frontOfFilename))) results.Add(file);
+            }
+            return results;
         }
     }
  }
