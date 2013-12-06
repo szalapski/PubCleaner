1f60c1d5d0b53c163f4c0acbcc42cac2cc918462
PSzalapski <szalap@hotmail.com>
2013-12-05 22:30:50 -0600

    Starting stubs

---
+++ PubCleaner.Lib/CleanResults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Szalapski.PubCleaner.Lib {
+    public class CleanResults {
+        public CleanResults(bool success) {
+            Success = success;
+        }
+
+        public bool Success { get; set; }
+    }
+}
--- PubCleaner.Lib/Cleaner.cs
+++ PubCleaner.Lib/Cleaner.cs
@@ -1,5 +1,11 @@
 ﻿namespace Szalapski.PubCleaner.Lib {
     public class Cleaner {
-
+        
+        /// <summary>
+        /// Cleans all old periodcials/publications from the attached Kindle, leaving only the most recent one
+        /// </summary>
+        public CleanResults Clean() {
+            return new CleanResults(true);
+        }
     }
-}
+ }
