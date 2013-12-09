52157fe59e37f244f071f00d59bb34b7ee8655cb
PSzalapski <szalap@hotmail.com>
2013-12-08 19:53:57 -0600

    built out Get integration tests

---
+++ PubCleaner.Lib/IPeriodicalStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szalapski.PubCleaner.Lib {
+    
+    /// <summary>
+    /// Implementations represent the files on an attached kindle and provide functions for working with them
+    /// </summary>
+    public interface IPeriodicalStore {
+        IEnumerable<FileInfo> GetPeriodicals();
+        void Delete(FileInfo periodical);
+        void Delete(IEnumerable<FileInfo> periodicals);
+    }
+}
---
+++ PubCleaner.Lib/KindlePeriodicalStore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Szalapski.PubCleaner.Lib {
+
+    /// <summary>
+    /// Represents a Kindle's stored periodicals.
+    /// </summary>
+    public class KindlePeriodicalStore : IPeriodicalStore {
+
+        public KindlePeriodicalStore(DirectoryInfo kindleRootDirectory) {
+            if (kindleRootDirectory == null) throw new ArgumentNullException("kindleRootDirectory");
+            root = kindleRootDirectory;
+        }
+        private DirectoryInfo root;
+
+        public IEnumerable<FileInfo> GetPeriodicals() {
+            return root.EnumerateFiles("*.pobi", SearchOption.AllDirectories);
+        }
+
+        public void Delete(System.IO.FileInfo periodical) {
+            periodical.Delete();
+        }
+
+        public void Delete(IEnumerable<System.IO.FileInfo> periodicals) {
+            foreach (FileInfo periodical in periodicals) Delete(periodical);
+        }
+    }
+}
