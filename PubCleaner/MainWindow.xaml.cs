e434b6026f42808fa3dca15df5fe90f65931d0ff
PSzalapski <szalap@hotmail.com>
2013-12-15 16:56:56 -0600

    wired up quick and dirty UI

--- PubCleaner/MainWindow.xaml.cs
+++ PubCleaner/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Szalapski.PubCleaner.Lib;
+using WpfControls;
 
 namespace Szalapski.PubCleaner {
     /// <summary>
@@ -20,6 +23,25 @@ namespace Szalapski.PubCleaner {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            CleanNowButton.Click += new RoutedEventHandler((x, y) => this.CleanNow());  //TODO: binding
+        }
+
+        public string CleanResults { get; set; }
+
+        private void CleanNow() {
+            //TODO: make instancy
+            IEnumerable<DirectoryInfo> directories = KindleDetector.DetectKindleDirectories();
+                StringBuilder resultsBuilder = new StringBuilder();
+            foreach (DirectoryInfo kindleDirectory in directories){
+                var cleaner = new Cleaner(new KindlePeriodicalStore(kindleDirectory, pretendDelete:true));
+                var results = cleaner.Clean();
+                foreach (FileInfo file in results.FilesCleaned){
+                    resultsBuilder.AppendLine(string.Format("Deleted {0}", file.FullName));
+                }
+            }
+            CleanResults = resultsBuilder.ToString();
+            ResultsTextBlock.Text = CleanResults; // todo: binding
+            
         }
     }
 }
