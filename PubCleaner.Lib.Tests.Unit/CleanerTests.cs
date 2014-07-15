57d41c2851e07e5dc504ac36b57c679db97042cb
szalapski <pszalapski@gmail.com>
2014-07-15 12:35:45 -0500

    more git demo

--- PubCleaner.Lib.Tests.Unit/CleanerTests.cs
+++ PubCleaner.Lib.Tests.Unit/CleanerTests.cs
@@ -14,7 +14,7 @@ namespace Szalapski.PubCleaner.Lib.Tests.Unit {
             var mockKindle = new Mock<IPeriodicalStore>();
             var sut = new SingleCleaner(mockKindle.Object);
             var result = sut.Clean();
-            // wilson demo
+            // wilson demo 2
         }
 
         [TestMethod]
